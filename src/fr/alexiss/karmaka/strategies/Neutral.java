cab90cb94618180c71cc3a76ff1b469b61298016
Zalko <88582103+Zalk0@users.noreply.github.com>
2023-12-22 12:07:18 +0100

    feat: implement neutral strategy

--- src/fr/alexiss/karmaka/strategies/Neutral.java
+++ src/fr/alexiss/karmaka/strategies/Neutral.java
@@ -1,10 +1,20 @@
 package fr.alexiss.karmaka.strategies;
 
 import fr.alexiss.karmaka.BotPlayer;
+import fr.alexiss.karmaka.Menu;
+import fr.alexiss.karmaka.cards.Card;
 
 public class Neutral implements Strategy {
     @Override
     public void play(BotPlayer player) {
-        //TODO implement turn
+        switch (Menu.getInstance().getRandom().nextInt(3)) {
+            case 0 -> player.addToDeeds(player.getHand().removeRandom());
+            case 1 -> {
+                Card card = player.getHand().removeRandom();
+                card.ability();
+                Menu.getInstance().getGame().getPlayers().get((Menu.getInstance().getGame().getPlayers().indexOf(player) + 1) % 2).takeCard(card);
+            }
+            case 2 -> player.addToFutureLife(player.getHand().removeRandom());
+        }
     }
 }
