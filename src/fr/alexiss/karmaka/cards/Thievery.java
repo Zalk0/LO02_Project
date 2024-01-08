b54639ec969d93fd4e2b6d800c9dc8330905dedd
PetiteGrange <134615787+PetiteGrange@users.noreply.github.com>
2024-01-08 10:54:32 +0100

    Implements thivery

--- src/fr/alexiss/karmaka/cards/Thievery.java
+++ src/fr/alexiss/karmaka/cards/Thievery.java
@@ -1,5 +1,7 @@
 package fr.alexiss.karmaka.cards;
 
+import fr.alexiss.karmaka.Menu;
+import fr.alexiss.karmaka.Pile;
 import fr.alexiss.karmaka.enums.CardColor;
 
 public class Thievery extends Card {
@@ -10,6 +12,15 @@ public class Thievery extends Card {
 
     @Override
     public void ability() {
-
+    	Pile<Card> playerHand = Menu.getInstance().getGame().getCurrentPlayer().getHand();
+    	Pile<Card> rivalDeeds = Menu.getInstance().getGame().getOppositePlayer().getDeeds();
+    	
+    	try {
+			Card carte = rivalDeeds.getFirst();
+			playerHand.add(carte);
+			System.out.println("L'Oeuvre " + carte + "a été volée!");
+		} catch (Exception e){
+	        System.out.println("Le rival n'a pas d'oeuvre!");
+		}
     }
 }
