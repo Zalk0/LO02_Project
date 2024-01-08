faa401d589654930a972194877e601dec2a8baff
PetiteGrange <134615787+PetiteGrange@users.noreply.github.com>
2024-01-08 11:13:42 +0100

    Implements Vengeance and Transmigrate

--- src/fr/alexiss/karmaka/cards/Transmigrate.java
+++ src/fr/alexiss/karmaka/cards/Transmigrate.java
@@ -1,5 +1,8 @@
 package fr.alexiss.karmaka.cards;
 
+import fr.alexiss.karmaka.Menu;
+import fr.alexiss.karmaka.Pile;
+import fr.alexiss.karmaka.Player;
 import fr.alexiss.karmaka.enums.CardColor;
 
 public class Transmigrate extends Card {
@@ -10,6 +13,33 @@ public class Transmigrate extends Card {
 
     @Override
     public void ability() {
-
+    	Pile<Card> playerFutureLife = Menu.getInstance().getGame().getCurrentPlayer().getFutureLife();
+    	Player player = Menu.getInstance().getGame().getCurrentPlayer();
+    	boolean chosen = false;
+    	
+    	System.out.println("Carte(s) présente(s) dans votre Vie Future:\n");
+        for (int i = 0; i < playerFutureLife.size(); i++) {
+            System.out.println((i + 1) + ". " + playerFutureLife.get(i));
+        }
+        
+        while (chosen == false) {
+	        System.out.println("\nSélectionner une carte par son numéro:");
+	        
+	        int choice = player.getChoice(0, playerFutureLife.size());
+	        
+	        Card cardSelected = playerFutureLife.get(choice - 1);
+	        System.out.println("\n" + cardSelected.getDetails());
+	
+	        System.out.println("\nChoisir une action:");
+	        System.out.println("0. Retour");
+	        System.out.println("1. Prendre en main");
+	        
+	        if (choice == 1) {
+	        	chosen = true;
+	        	System.out.println("Je prend " + cardSelected + ".");
+	        	playerFutureLife.remove(cardSelected);
+                player.getHand().add(cardSelected);
+	        }
+        }
     }
 }
--- src/fr/alexiss/karmaka/cards/Vengeance.java
+++ src/fr/alexiss/karmaka/cards/Vengeance.java
@@ -1,5 +1,6 @@
 package fr.alexiss.karmaka.cards;
 
+import fr.alexiss.karmaka.*;
 import fr.alexiss.karmaka.enums.CardColor;
 
 public class Vengeance extends Card {
@@ -10,6 +11,15 @@ public class Vengeance extends Card {
 
     @Override
     public void ability() {
+    	Pile<Card> fosse = Menu.getInstance().getGame().getRuins();
+    	Pile<Card> rivalDeeds = Menu.getInstance().getGame().getOppositePlayer().getDeeds();
 
+    	try {
+			Card carte = rivalDeeds.getFirst();
+			fosse.add(carte);
+			System.out.println("L'Oeuvre " + carte + "a été défaussée!");
+		} catch (Exception e){
+	        System.out.println("Le rival n'a pas d'oeuvre!");
+		}
     }
 }
