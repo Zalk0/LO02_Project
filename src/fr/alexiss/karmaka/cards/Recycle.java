2bd89890caa4f80bbedb062d3dfe76fede5d928f
PetiteGrange <134615787+PetiteGrange@users.noreply.github.com>
2024-01-08 10:17:55 +0100

    Debut recycle

--- src/fr/alexiss/karmaka/cards/Recycle.java
+++ src/fr/alexiss/karmaka/cards/Recycle.java
@@ -1,5 +1,6 @@
 package fr.alexiss.karmaka.cards;
 
+import fr.alexiss.karmaka.*;
 import fr.alexiss.karmaka.enums.CardColor;
 
 public class Recycle extends Card {
@@ -10,6 +11,22 @@ public class Recycle extends Card {
 
     @Override
     public void ability() {
-
+    	Player player = Menu.getInstance().getGame().getCurrentPlayer();
+    	//Get 3 dernière de la Fosse
+    	Pile<Card> fosse = Menu.getInstance().getGame().getRuins();
+    	System.out.println("Trois dernières cartes de la défausse:\n");
+        for (int i = 0; i < 3; i++) {
+        	try {
+        		System.out.println((i + 1) + ". " + fosse.get(i));
+        	} catch (Exception e) {
+        		break;
+        	}
+        }
+    	
+    	//Choix de la carte
+        //TODO JE sais plus comment on fait
+    	
+    	//Ajout d'une carte
+    	
     }
 }
